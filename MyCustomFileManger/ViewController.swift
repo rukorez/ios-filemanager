c9c14413bdd1c08d2fd023b533c8472c1c7eb8e8
rukorez <77271521+rukorez@users.noreply.github.com>
2022-09-05 17:54:54 +0300

    Initial Commit

---
+++ MyCustomFileManger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCustomFileManger
+//
+//  Created by Филипп Степанов on 05.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
